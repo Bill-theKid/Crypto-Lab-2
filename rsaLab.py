0bf17ff5a307f58a2ac889838c894b873c2ad0b8
Pearce <billpearce25@gmail.com>
2023-03-22 16:45:21 -0500

    encrypt/decrypt

--- rsaLab.py
+++ rsaLab.py
@@ -1,3 +1,5 @@
+import sys
+
 p = 137 # prime
 q = 131 # prime
 n = p * q
@@ -16,5 +18,30 @@ print(f' n = {n}\n r = {r}\n k = {k} \n d = {d}')
 with open('message.txt', 'rb') as infile:
     with open('cipher.txt', 'wb') as outfile:
         data = infile.read(2)
+        print('encrypting...')
         while data:
+            print(data)
             # encrypt
+            msg_block = int.from_bytes(data, sys.byteorder)
+            cipher_txt = (msg_block ** e) % n
+            print(cipher_txt)
+            out_block = cipher_txt.to_bytes(2, sys.byteorder)
+            outfile.write(out_block)
+            data = infile.read(2)
+
+with open('cipher.txt', 'rb') as infile:
+    with open('decrypted.txt', 'wb') as outfile:
+        data = infile.read(2)
+        print('decrypting...')
+        while data:
+            # decrypt
+            msg_block = int.from_bytes(data, sys.byteorder)
+            for x in range(d - 1):
+                msg_block = msg_block * msg_block % n
+            decrypt_txt = msg_block
+            print(decrypt_txt)
+            out_block = decrypt_txt.to_bytes(2, sys.byteorder)
+            print(out_block)
+            outfile.write(out_block)
+            data = infile.read(2)
+
