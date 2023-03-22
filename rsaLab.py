1cda6770647bc38628897dcbf881acf3915b6d61
William Pearce <billpearce25@gmail.com>
2023-03-22 11:41:42 -0500

    started encryption

--- rsaLab.py
+++ rsaLab.py
@@ -6,9 +6,15 @@ r = (p-1) * (q-1)
 k = r + 1 # first number equal to 1 mod r
 d = k / e # divide by e and check validity vv
 
-while((e*d % r) != 1 or (d % 1) != 0):
+while((e*d % r) != 1 or (d % 1) != 0): # check if coprime and if d is integer
     k = k + r
     d = k / e
 d = int(d)
 
-print(f'n = {n}\nr = {r}\nk = {k} \nd = {d}')
+print(f' n = {n}\n r = {r}\n k = {k} \n d = {d}')
+
+with open('message.txt', 'rb') as infile:
+    with open('cipher.txt', 'wb') as outfile:
+        data = infile.read(2)
+        while data:
+            # encrypt
