8bf7d35d6576cdee4d9dc6534e22b515e8799f8d
Pearce <billpearce25@gmail.com>
2023-03-27 16:45:48 -0500

    multithread

--- server/server.py
+++ server/server.py
@@ -2,7 +2,34 @@ import socket
 # import jks
 import hashlib
 import rsa
+import threading
 
+def thread_task(client, address):
+    print(f'connection from {address}')
+
+    print('sending public key...')
+    with open('server_public', 'rb') as key:
+        client.sendall(key.read())
+
+    print('receiving public key...')
+    with open('client_public', 'wb') as key:
+        key.write(client.recv(1024))
+
+    sigmsg = client.recv(1024)
+    sig = sigmsg[:64]
+    ciphertxt = sigmsg[64:]
+    sent_hash = rsa.decrypt(sig, 'client_public')
+    plaintxt = rsa.decrypt(ciphertxt, 'server_private')
+    local_hash = hashlib.sha256(plaintxt).digest()
+
+    if local_hash.hex() == sent_hash.hex():
+        print('Message Verified!')
+        print(plaintxt.decode())
+    else:
+        print('Invalid digital signature, discarding message.')
+        
+    return
+                
 # keystore = jks.KeyStore.new('jks', [])
 # keystore.save('server.jks', 'password1234')
 
@@ -12,28 +39,7 @@ PORT = 7791
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    client, address = s.accept()
-    with client:
-        print(f'connection from {address}')
-
-        print('sending public key...')
-        with open('server_public', 'rb') as key:
-            client.sendall(key.read())
-
-        print('receiving public key...')
-        with open('client_public', 'wb') as key:
-            key.write(client.recv(1024))
-
-        sigmsg = client.recv(1024)
-        sig = sigmsg[:64]
-        ciphertxt = sigmsg[64:]
-        sent_hash = rsa.decrypt(sig, 'client_public')
-        plaintxt = rsa.decrypt(ciphertxt, 'server_private')
-        local_hash = hashlib.sha256(plaintxt).digest()
-
-        if local_hash.hex() == sent_hash.hex():
-            print('Message Verified!')
-            print(plaintxt.decode())
-        else:
-            print('Invalid digital signature, discarding message.')
+    while True:
+        client, address = s.accept()
+        threading.Thread(target=thread_task, args=(client, address)).start()
         
